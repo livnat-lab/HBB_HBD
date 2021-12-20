48a1832d8f3479fc4154daf1ac31c98f223157a0
livnat-lab <61454237+livnat-lab@users.noreply.github.com>
2021-12-20 23:58:59 +0200

    Update params_1.sh

--- MEMDS_v1.1/design/params_1.sh
+++ MEMDS_v1.1/design/params_1.sh
@@ -1,13 +1,13 @@
 
 ### Activate Conda module ############
-. /data/home/programs/miniconda2/etc/profile.d/conda.sh
+. /path/to/miniconda2/etc/profile.d/conda.sh
 conda activate modules3
 #############################
 
 # Set work directories
 params_adapters_1="$PWD/wildcard_adapters_1.fa"
-params_dir_out_1='/data/home/run15_1and2/out1'
-params_dir_reference='/data/home/run15_1and2/seqs'
+params_dir_out_1='/data/home/My_experiment/out1'
+params_dir_reference='/data/home/My_experiment/seqs'
 
 # Set run parameters
 params_ninimum_fastq_size_1=80
